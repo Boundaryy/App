6cfd8fa785edcf3cf99f7d5d1d1f9eedde3de7c1
huise0ng <shinhuiseong07@gmail.com>
2024-08-25 15:15:38 +0900

    💄 :: 시작 페이지

--- App.js
+++ App.js
@@ -1,11 +1,23 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { StyleSheet, Text, View, Image, TouchableOpacity } from 'react-native';
+import { useRouter } from 'expo-router';
 
-export default function App() {
+export default function Index() {
+  const router = useRouter();
+
+  const handleSignupClick = () => {
+    router.push('/choose');
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Hello world your app!</Text>
-      <StatusBar style="auto" />
+      <Image source={require('../assets/images/logo.svg')} style={styles.logo} />
+
+      <TouchableOpacity style={styles.signupButton} onPress={handleSignupClick}>
+        <Text style={styles.signupButtonText}>회원가입</Text>
+      </TouchableOpacity>
+
+      <Text style={styles.footer}>©Barder</Text>
     </View>
   );
 }
@@ -13,8 +25,35 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  logo: {
+    width: 140,
+    height: 'auto',
+    position: 'absolute',
+    top: 350,
+  },
+  signupButton: {
+    width: 310,
+    height: 50,
+    backgroundColor: '#5772FF',
+    borderRadius: 8,
+    justifyContent: 'center',
+    alignItems: 'center',
+    position: 'absolute',
+    bottom: 100,
+  },
+  signupButtonText: {
+    color: '#FFFFFF',
+    fontSize: 18,
+    fontFamily: 'Pretendard',
+    fontWeight: '600',
+  },
+  footer: {
+    position: 'absolute',
+    bottom: 60,
+    fontSize: 14,
+    color: '#808080',
   },
 });
