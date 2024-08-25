b1c59bc4e0043cec8a1f0cd4d38e7661764ea631
huise0ng <shinhuiseong07@gmail.com>
2024-08-25 17:40:55 +0900

    💄 :: /choose 퍼블리싱

--- App.js
+++ App.js
@@ -1,59 +1,18 @@
 import React from 'react';
-import { StyleSheet, Text, View, Image, TouchableOpacity } from 'react-native';
-import { useRouter } from 'expo-router';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import Index from './app/index.jsx';
+import Choose from './app/choose.jsx'; 
 
-export default function Index() {
-  const router = useRouter();
+const Stack = createNativeStackNavigator();
 
-  const handleSignupClick = () => {
-    router.push('/choose');
-  };
-
+export default function App() {
   return (
-    <View style={styles.container}>
-      <Image source={require('../assets/images/logo.svg')} style={styles.logo} />
-
-      <TouchableOpacity style={styles.signupButton} onPress={handleSignupClick}>
-        <Text style={styles.signupButtonText}>회원가입</Text>
-      </TouchableOpacity>
-
-      <Text style={styles.footer}>©Barder</Text>
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Index">
+        <Stack.Screen name="Index" component={Index} options={{ title: 'Home' }} />
+        <Stack.Screen name="Choose" component={Choose} options={{ title: 'Choose' }} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  logo: {
-    width: 140,
-    height: 'auto',
-    position: 'absolute',
-    top: 350,
-  },
-  signupButton: {
-    width: 310,
-    height: 50,
-    backgroundColor: '#5772FF',
-    borderRadius: 8,
-    justifyContent: 'center',
-    alignItems: 'center',
-    position: 'absolute',
-    bottom: 100,
-  },
-  signupButtonText: {
-    color: '#FFFFFF',
-    fontSize: 18,
-    fontFamily: 'Pretendard',
-    fontWeight: '600',
-  },
-  footer: {
-    position: 'absolute',
-    bottom: 60,
-    fontSize: 14,
-    color: '#808080',
-  },
-});
