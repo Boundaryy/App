9a897b23f5802fbe9345753445bcb93927005d02
huise0ng <shinhuiseong07@gmail.com>
2024-08-29 15:03:43 +0900

    ADD :: 세팅

--- App.js
+++ App.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import Index from './app/index.jsx';
-import Choose from './app/choose.jsx';
-import SignIn from './app/guardian/signin.jsx'; 
+import Index from './index.jsx';
+import Choose from './choose.jsx';
+import SignIn from './guardian/signin.jsx';
+import ChildNav from './nav/childnav';  // 수정된 네비게이션 바 컴포넌트 임포트
 
 const Stack = createNativeStackNavigator();
 
@@ -13,8 +14,9 @@ export default function App() {
       <Stack.Navigator initialRouteName="Index">
         <Stack.Screen name="Index" component={Index} options={{ title: 'Home' }} />
         <Stack.Screen name="Choose" component={Choose} options={{ title: 'Choose' }} />
-        <Stack.Screen name="SignIn" component={SignIn} options={{ title: 'Sign In' }} /> {/* SignIn 화면 추가 */}
+        <Stack.Screen name="SignIn" component={SignIn} options={{ title: 'Sign In' }} />
       </Stack.Navigator>
+      <ChildNav />  
     </NavigationContainer>
   );
 }
