3561ed993f2262d8f3c0f3a7fb5ee5bf840b2f5f
huise0ng <shinhuiseong07@gmail.com>
2024-09-09 08:28:25 +0900

    🐛 Text 크기 수정

--- app/index.jsx
+++ app/index.jsx
@@ -1,27 +1,27 @@
-import React from 'react';
-import { globalStyles } from '../styles/global'
+import React, { useState } from 'react';
+import { globalStyles } from '../styles/global';
 import { StyleSheet, Text, View, Image, TouchableOpacity } from 'react-native';
 import { useRouter } from 'expo-router';
-import { Button } from '../components/Button.jsx'
-
+import { Button } from '../components/Button.jsx';
 
 export default function Index() {
-  const router = useRouter()
+  const router = useRouter();
+  const [fontSize, setFontSize] = useState(16);  // 초기 글자 크기 설정
 
   const handle = () => {
-    router.push('/choose')
-  }
+    router.push('/choose');
+  };
 
   const chooseLogin = () => {
-    router.push('/chooselogin')
-  }
+    router.push('/chooselogin');
+  };
 
   return (
     <View style={globalStyles.container}>
       <Image source={require('../assets/logo.png')} style={styles.logo} />
-      <Button onPress={handle} title={"회원가입"}/>
-      <TouchableOpacity onPress={chooseLogin} style={{margin:20}}>
-        <Text style={{fontWeight:400}}>로그인</Text>
+      <Button onPress={handle} title={"회원가입"} />
+      <TouchableOpacity onPress={chooseLogin} style={{ margin: 20 }}>
+        <Text style={[styles.loginText, { fontSize }]} onPress={() => setFontSize(fontSize + 2)}>로그인</Text>
       </TouchableOpacity>
       <Text style={globalStyles.footer}>©Barder</Text>
     </View>
@@ -30,8 +30,13 @@ export default function Index() {
 
 const styles = StyleSheet.create({
   logo: {
-    width: 200,
+    width: 170,
     marginBottom: 100,
-    height: 146,
+    height: 126,
+  },
+  loginText: {
+    fontWeight: '400',
+    fontSize: 16,
+    color: 'gray',
   },
 });
