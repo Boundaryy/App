e60976ac632e38fd0aa2a26bb233cbaf4efa4c63
huise0ng <shinhuiseong07@gmail.com>
2025-01-21 20:02:03 +0900

    STYLE :: 모든 글자 프리텐다드 폰트 적용

--- styles/global.js
+++ styles/global.js
@@ -3,51 +3,63 @@ import { StyleSheet } from "react-native";
 export const globalStyles = StyleSheet.create({
   container: {
     flex: 1,
-    width: '100%', 
+    width: '100%',
+    fontFamily: 'Pretendard',
   },
   logincontainer: {
-    width:"100%",
+    width: "100%",
     height: "100%",
     display: "flex",
     justifyContent: "flex-end",
     alignItems: "center",
-    paddingBottom:20,
+    paddingBottom: 20,
+    fontFamily: 'Pretendard',
   },
   footer: {
     position: 'absolute',
     bottom: 40,
     fontSize: 14,
     color: '#808080',
+    fontFamily: 'Pretendard',
   },
   header: {
     padding: 15,
-    paddingTop: 20, // 상단 패딩 줄임
+    paddingTop: 20,
+    fontFamily: 'Pretendard',
   },
   title: {
-    fontSize: 16, // 폰트 크기 줄임
+    fontSize: 16,
     fontWeight: 'bold',
     color: '#5772FF',
+    fontFamily: 'Pretendard',
   },
   subtitle: {
-    fontSize: 26, // 폰트 크기 줄임
-    whiteSpace:"nowrap",
+    fontSize: 26,
+    whiteSpace: "nowrap",
     fontWeight: '700',
+    marginLeft: 20,
+    fontFamily: 'Pretendard',
   },
   description: {
-    fontSize: 15, // 폰트 크기 줄임
+    fontSize: 16,
     textAlign: 'left',
     color: '#898989',
-    marginBottom: 10, // 하단 마진 줄임
+    marginBottom: 10,
     marginTop: -10,
+    marginLeft: 20,
+    fontFamily: 'Pretendard',
+    letterSpacing: -0.5, 
   },
   formGroup: {
     width: '100%',
     paddingHorizontal: 20,
     marginBottom: 20,
+    fontFamily: 'Pretendard',
   },
   backText: {
     fontSize: 18,
     color: '#808080',
+    fontFamily: 'Pretendard',
   },
   backButton: {
     position: 'absolute',
@@ -59,6 +71,7 @@ export const globalStyles = StyleSheet.create({
     fontWeight: '400',
     marginBottom: 5,
     textAlign: 'left',
+    fontFamily: 'Pretendard',
   },
   input: {
     padding: 10,
@@ -67,5 +80,6 @@ export const globalStyles = StyleSheet.create({
     fontSize: 18,
     color: '#5772FF',
     width: '100%',
+    fontFamily: 'Pretendard',
   },
 });
