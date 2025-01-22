239f002d17ad5df5ced22a75af5b59bf636ddcdd
huise0ng <shinhuiseong07@gmail.com>
2025-01-22 13:15:21 +0900

    FIX :: Button 컴포넌트 렌더링 안되는 문제 수정

--- components/Button.jsx
+++ components/Button.jsx
@@ -1,27 +1,25 @@
-
 import { View, StyleSheet, Text, TouchableOpacity } from 'react-native';
 import React from 'react';
 import { useRouter } from 'expo-router';
-import axios from 'axios';
 
-export const Button = ({onPress, title}) => {
+const Button = ({ onPress, title }) => {
   const router = useRouter();
 
   return (
     <View style={styles.buttonContainer}> 
-    <TouchableOpacity style={styles.button} onPress={onPress}>
-      <Text style={styles.buttonText}>{title}</Text>
-    </TouchableOpacity>
+      <TouchableOpacity style={styles.button} onPress={onPress}>
+        <Text style={styles.buttonText}>{title}</Text>
+      </TouchableOpacity>
     </View>
-  )
-}
+  );
+};
 
 const styles = StyleSheet.create({
-  buttonContainer:{
-      width: "100%",
-      display: "flex",
-      justifyContent: "center",
-      alignItems: "center",
+  buttonContainer: {
+    width: '100%',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
   },
   button: {
     width: 310,
@@ -29,12 +27,15 @@ const styles = StyleSheet.create({
     backgroundColor: '#5772FF',
     borderRadius: 8,
     justifyContent: 'center',
-    alignItems: 'center',
+    alignItems: 'center', 
+    marginBottom: -180,
   },
   buttonText: {
     color: '#FFFFFF',
     fontSize: 18,
     fontFamily: 'Pretendard',
     fontWeight: '600',
-  }
+  },
 });
+
+export default Button;
